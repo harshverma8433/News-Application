d3a5bd0658687ef51e43057412d91ab3024368ea
verma8433 <hv77856@gmail.com>
2024-07-05 10:00:23 +0530

    dangerous html file

--- client/src/Components/ArticleSection/LikedArticle/LikedArticle.jsx
+++ client/src/Components/ArticleSection/LikedArticle/LikedArticle.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import toast, { Toaster } from 'react-hot-toast';
+import  { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 const LikedArticle = () => {
